e4b4f12086a12768b3a2917bb1abcb9906649b2f
Pragathiswaran <mpragathiswaran@gmail.com>
2024-06-08 20:02:02 +0530

    commit

---
+++ form/server/server.js
@@ -0,0 +1,12 @@
+// backend/server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send("Hello from the server!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
