136b0443bb7f9f20a06f022287c451a9e5360952
Pragathiswaran <mpragathiswaran@gmail.com>
2024-06-13 12:31:53 +0530

    commit

--- form/client/src/App.jsx
+++ form/client/src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Button, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
 import './App.css'
 function App() {
   return (
--- form/server/server.js
+++ form/server/server.js
@@ -1,72 +1,11 @@
 const express = require('express');
 const app = express();
-const { MongoClient } = require("mongodb");
-// const dotenv = require('dotenv');
-
-// dotenv.config();  // Load environment variables from .env file  process.env.MONGODB_URI || 
-
+// const { MongoClient } = require("mongodb");
 const PORT = process.env.PORT || 8000;
-const uri = 'mongodb://localhost:27017';
-const dbName = 'register';
 
-async function createUniqueIndex() {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  try {
-    await client.connect();
-    const db = client.db(dbName);
-    const collection = db.collection('signup');
-
-    // Create a unique index on the email field
-    await collection.createIndex({ email: 1 }, { unique: true });
-
-    console.log("Unique index created on 'email' field.");
-  } catch (error) {
-    console.error('Error creating unique index:', error);
-  } finally {
-    await client.close();
-  }
-}
-
-// Create the unique index when the server starts
-createUniqueIndex();
-
-app.get('/api', async (req, res) => {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-
-  await client.connect();
-  const db = client.db(dbName);
-  const collection = db.collection('signup');
-
-  try {
-  
-    // Insert a document
-    await collection.insertOne({
-      id: 1,
-      username: 'mihaan',
-      email: 'mihaan89@gmail.com',
-      password: 'mihaan@009'
-    });
-   
-    res.status(200).json({
-      message: 'API is working and the data is stored in database'
-    });
-  } catch (error) {
-    console.error('Error connecting to MongoDB or inserting document:', error);
-    if (error.code === 11000) {
-      const value = collection.find({})
-      res.status(400).json({
-        message: 'Duplicate key error: Email must be unique',
-        value: value
-      });
-    } else {
-      res.status(500).json({
-        message: 'Internal Server Error'
-      });
-    }
-  } finally {
-    await client.close();
-  }
-});
+app.get('/api',(req,res)=>{
+  res.send('Hello World')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
