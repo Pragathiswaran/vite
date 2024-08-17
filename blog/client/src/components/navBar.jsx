886383d74fb0c284644f26f2b2e8b331c5bf1ddb
Pragathiswaran <mpragathiswaran@gmail.com>
2024-08-17 11:59:06 +0530

    feat : install material tailwind

--- blog/client/src/components/navBar.jsx
+++ blog/client/src/components/navBar.jsx
@@ -4,19 +4,7 @@ import { Link } from 'react-router-dom'
 function navBar() {
   return (
     <>
-      <nav className='text-gray-700 flex justify-between p-5 drop-shadow-xl bg-fuchsia-50 z[1000]'>
-        <div className='pl-10'>
-            logo
-        </div>
-        <div className='flex gap-x-20'>
-            <Link to='/'>Home</Link>
-            <Link to='/'>About</Link>
-            <Link to='/'>Contact</Link>
-        </div>
-        <div className='pr-10'>
-            profile
-        </div>
-      </nav>
+
     </>
   )
 }
