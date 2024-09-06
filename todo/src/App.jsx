401d3ba6363aeb165048f92bb85e94f58fbc1453
Pragathiswaran <mpragathiswaran@gmail.com>
2024-09-06 21:18:21 +0530

    feat : created the navbar

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,22 +1,19 @@
-import React , {useState} from "react"
-import { AppBar, Button } from '@mui/material'
-import { motion } from 'framer-motion'
+import React from "react"
+import { AppBar, Toolbar, Fab } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add';
 import './App.css'
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <AppBar position='fixed' color='primary' sx={{top:'96%'}}>
-        <Button varient="outlined" color="inherit" size="small" onClick={() => setIsOpen(!isOpen)}>click</Button>
-      </AppBar>
-      <div>
-      <motion.div
-        layout
-        data-isOpen={isOpen}
-        initial={{ borderRadius: 50 }}
-        className="parent"
-      ></motion.div>
-      </div>
+      {/* <AppBar position='fixed' color='primary' sx={{ top:'91.07%',display:'flex', alignItems:'center'}}>
+       <Toolbar>
+        <Fab color="inform" aria-label="add">
+          <AddIcon />
+        </Fab>
+       </Toolbar>
+      </AppBar> */}
+      <div className="top"></div>
+      <div className="bottom"></div>
     </>
   )
 }
