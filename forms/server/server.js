62d2929bdac9692c9434f1d7089f685e597bf5bb
Pragathiswaran <mpragathiswaran@gmail.com>
2024-10-13 15:55:44 +0530

    feat : added login page

--- forms/server/server.js
+++ forms/server/server.js
@@ -6,10 +6,18 @@ dotenv.config({path:path.resolve(__dirname, '.env')})
 const mongoose = require('mongoose')
 const cors = require('cors')
 
+const routes = require('./routes/authRoutes')
+
 app.use(express.json())
 app.use(cors())
 
 const port = process.env.PORT
+
+mongoose.connect(process.env.DB)
+.then(()=> console.log('Database conncted Successfully'))
+.catch(err=>console.log(`Connection Failed ${err}`))
+
+app.use('/api',routes)
 
 app.listen(port,(req,res)=>{
     console.log(`Server is listening to port ${port}`)
