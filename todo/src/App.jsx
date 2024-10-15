6bc6b4848a197c49b99c694c3749597a9740a780
Pragathiswaran <mpragathiswaran@gmail.com>
2024-10-15 21:46:53 +0530

    feat : added header footer and main section

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,19 +1,64 @@
 import React from "react"
-import { AppBar, Toolbar, Fab } from '@mui/material'
-import AddIcon from '@mui/icons-material/Add';
-import './App.css'
+import "./App.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 function App() {
   return (
     <>
-      {/* <AppBar position='fixed' color='primary' sx={{ top:'91.07%',display:'flex', alignItems:'center'}}>
-       <Toolbar>
-        <Fab color="inform" aria-label="add">
-          <AddIcon />
-        </Fab>
-       </Toolbar>
-      </AppBar> */}
-      <div className="top"></div>
-      <div className="bottom"></div>
+    <header className="fixed top-0 w-full">
+      <nav className="py-4 bg-blue-500 text-center mb-20">
+        <span className="text-white text-3xl">TODO LIST</span>
+      </nav>
+    </header>
+    <main className="mt-28">
+      <section className="flex flex-wrap justify-around px-10 py-5">
+        <div className="flex justify-center w-1/4 h-40 border border-black m-3">
+          
+        </div>
+        <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+          </div>
+          <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+          </div>
+          <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+          </div>
+          <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+          </div>
+          <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+          </div>
+          <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+        </div>
+        <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+          </div>
+          <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+          </div>
+          <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+          </div>
+          <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+          </div>
+          <div className="flex justify-center w-1/4 h-32 border border-black m-3">
+          
+          </div>
+        
+      </section>
+    </main>
+   <footer className="fixed w-full bottom-0">
+      <div className="menu__border"></div>
+      <nav className="bg-blue-500 text-white w-full h-16 flex justify-center">
+        <button className="relative bottom-16 bg-white text-gray-950 text-2xl w-28 h-28 rounded-full ">
+        <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </nav>
+   </footer>
     </>
   )
 }
